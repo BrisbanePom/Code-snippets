8f833693bfc0454ca417e57296ffe02217f06272
BrisbanePom <smc.biz@gmail.com>
2021-02-22 15:55:42 +1030

    Update TidyverseStuff.R

--- TidyverseStuff.R
+++ TidyverseStuff.R
@@ -3,5 +3,8 @@
 
 #Utilities
 
+#Remove all objects from the environment
+rm(list = ls())
+
 #Copy dataframe to clipboard for pasting to Excel
 write.table(dfX, "clipboard-20000", sep="\t", row.names=FALSE) 
